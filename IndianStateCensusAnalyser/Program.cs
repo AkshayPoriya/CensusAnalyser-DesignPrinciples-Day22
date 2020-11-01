48ad008e0aa60e9ca5308ba1c51733e3a6710ef4
AkshayPoriya <akshayporiya1234@gmail.com>
2020-11-01 12:51:32 +0530

    initial commit

---
+++ IndianStateCensusAnalyser/Program.cs
@@ -0,0 +1,25 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Program.cs" company="Bridgelabz">
+//   Copyright © 2018 Company
+// </copyright>
+// <creator Name="Akshay Poriya"/>
+// --------------------------------------------------------------------------------------------------------------------
+namespace IndianStateCensusAnalyser
+{
+    using System;
+
+    /// <summary>
+    /// Contains Main function
+    /// </summary>
+    class Program
+    {
+        /// <summary>
+        /// Defines the entry point of the application.
+        /// </summary>
+        /// <param name="args">The arguments.</param>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
